2bd9c5a755c795b40876d90082021b4107412578
OrKeren8 <orkeren8@gmail.com>
2018-01-24 13:13:12 -0800

    upload project number 5 on dictionary

---
+++ 5th_project_dictionary.py
@@ -0,0 +1,21 @@
+dictionary = {}
+
+user_select = 0
+while user_select != 4:
+    print("insert the number of the task you want me to do\n"
+          "1) add a word to the dictionary\n"
+          "2) search for a word in the dictionary\n"
+          "3) delete a word from the dictionary\n"
+          "4) stop the program")
+    user_select = int(input())
+"""
+    if user_select == 1:
+        dictionary.update({input('the word in english'): input('the translation in hebrew')})
+    elif user_select == 2:
+        input('write the word you want to search:')
+        if
+        print()
+    elif user_select == 3:
+        print('3')
+print(dictionary)
+"""
