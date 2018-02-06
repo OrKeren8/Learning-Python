c2abc560e82756e83c4ef563b33a1d75b2b2df32
OrKeren8 <orkeren8@gmail.com>
2018-02-06 22:16:31 +0200

    update euler project task 54

--- euler_projects/euler_project_task_54/euler_project_task_54.py
+++ euler_projects/euler_project_task_54/euler_project_task_54.py
@@ -1,34 +1,8 @@
-from .detecting import RankDetector
-
+from euler_projects.euler_project_task_54.package.detecting import RankDetector
 left_hand_nums = []
 left_hand_suits = []
 right_hand_nums = []
 right_hand_suits = []
-
-
-def main():
-    with open('poker_hands.txt') as hands:
-        for line in hands:
-            list_of_line = convert_T_J_Q_K_A_to_nums(line)
-            # get the numbers and suits of each hand:
-            for i in range(0, 13, 3):
-                left_hand_nums.append(int(list_of_line[i]))
-                left_hand_suits.append(list_of_line[i+1])
-                right_hand_nums.append(int(list_of_line[i+15]))
-                right_hand_suits.append(list_of_line[i+16])
-            # call to the function that check the highest rank of the hand:
-            left_hand_rank = check_rank(left_hand_nums, left_hand_suits)
-            right_hand_rank = check_rank(right_hand_nums, right_hand_nums)
-            print(left_hand_rank)
-            print(right_hand_rank)
-            #
-            # need to take the ranks of hands and compare them
-            #
-            # clear hands for next round
-            right_hand_nums.clear()
-            right_hand_suits.clear()
-            left_hand_nums.clear()
-            left_hand_suits.clear()
 
 
 def convert_T_J_Q_K_A_to_nums(line):
@@ -52,12 +26,52 @@ def check_rank(hand_nums, hand_suits):  # call to the functions that gives the r
     rank_class = RankDetector(hand_nums, hand_suits)
     first_function_rank = rank_class.flush_straight_comb()
     second_function_rank = rank_class.x_of_a_kind()
-    if first_function_rank[0] > second_function_rank[4]:  # check which function found the best rank case in a hand
+    if first_function_rank[4] > second_function_rank[4]:  # check which function found the best rank case in a hand
         best_rank = first_function_rank
     else:
         best_rank = second_function_rank
     return best_rank
 
 
-if __name__ == "__main__":
-    main()
+def compere_func(index, left_hand_rank, right_hand_rank):
+    if left_hand_rank[index] > right_hand_rank[index]:
+        return 'left'
+    elif left_hand_rank[index] < right_hand_rank[index]:
+        return 'right'
+    else:
+        return None
+
+
+# def main():
+left = 0
+right = 0
+with open('poker_hands.txt') as hands:
+    for line in hands:
+        list_of_line = convert_T_J_Q_K_A_to_nums(line)
+        # get the numbers and suits of each hand:
+        for i in range(0, 13, 3):
+            left_hand_nums.append(int(list_of_line[i]))
+            left_hand_suits.append(list_of_line[i+1])
+            right_hand_nums.append(int(list_of_line[i+15]))
+            right_hand_suits.append(list_of_line[i+16])
+        # call to the function that check the highest rank of the hand:
+        left_hand_rank = check_rank(left_hand_nums, left_hand_suits)
+        right_hand_rank = check_rank(right_hand_nums, right_hand_suits)
+        winner = compere_func(4, left_hand_rank, right_hand_rank)
+        for i in range(1, 6, 2):
+            if not winner:
+                winner = compere_func(i, left_hand_rank, right_hand_rank)
+        if winner == 'left':
+            left += 1
+        else:
+            right += 1
+        # clear hands for next round
+        right_hand_nums.clear()
+        right_hand_suits.clear()
+        left_hand_nums.clear()
+        left_hand_suits.clear()
+
+print(left)
+print(right)
+# if __name__ == "__main__":
+#     main()
--- euler_projects/euler_project_task_54/package/detecting.py
+++ euler_projects/euler_project_task_54/package/detecting.py
@@ -3,7 +3,7 @@ class RankDetector(object):
     def __init__(self, hand_nums, hand_suits):
         self.hand_nums = list(hand_nums)
         self.hand_suits = list(hand_suits)
-        self.ranks = ['high card', 'one pair', 'two pairs', 'three of kind', 'straight', 'flush', 'full house', 'four of a kind', 'Straight Flush', 'royal flush']
+        self.ranks = ['high card', 'one pair', 'two pairs', 'three of a kind', 'straight', 'flush', 'full house', 'four of a kind', 'Straight Flush', 'royal flush']
 
     # a function that takes a hand of cards and checks if there is
     # straight/flush/straight flush/ straight royal.
@@ -21,23 +21,25 @@ class RankDetector(object):
             if self.hand_suits[i] == self.hand_suits[i+1]:
                 flush_check += 1
         if straight_check == 4 and flush_check == 4:
-            return self.ranks.index('Straight flush'), high_card
+            return 0, high_card, 0, 0, self.ranks.index('Straight flush'), high_card
         elif (straight_check == 3) and (self.hand_nums[3] == 5) and (self.hand_nums[4] == 14) and flush_check == 4:
             # in case of 1,2,3,4,5 and flush
-            return self.ranks.index('straight flush'), 5
+            return 0, 5, 0, 0, self.ranks.index('straight flush'), 5
         elif flush_check == 4:
-            return self.ranks.index('flush'), high_card
+            return 0, high_card, 0, 0, self.ranks.index('flush'), high_card
         elif straight_check == 4:
-            return self.ranks.index('straight'), high_card
+            return 0, high_card, 0, 0, self.ranks.index('straight'), high_card
         elif (straight_check == 3) and (self.hand_nums[3] == 5) and (self.hand_nums[4] == 14):
-            return self.ranks.index('straight'), 5
+            return 0, high_card, 0, 0, self.ranks.index('straight'), 5
+        else:
+            return 0, 0, 0, 0, 0, 0
     # a function that gets a hand of cards and checks if there is
     # pair/ two pairs/ 3 of a kind/ full house/ 4 of a kind
     # the function returns the best case it found
 
     def x_of_a_kind(self):
         current_hand_rank = []
-        best_hand_rank = []
+        best_hand_rank = [0, 0, 0, 0, 0, 0]
         high_card = self.hand_nums[4]  # the last card in the sorted hand has the highest value
         repeat_list = []  # a list that gets the times a number shows in a hand
         value_list = []  # a list that get the numbers that in a hand two times or more
@@ -49,18 +51,18 @@ class RankDetector(object):
         if len(repeat_list) < 1:  # if there is no duplicate numbers
             best_hand_rank = high_card
         elif len(repeat_list) < 2:  # if there is only one number that appears more then one time
-            repeat_list.append(None)
-            value_list.append(None)
+            repeat_list.append(0)
+            value_list.append(0)
             if repeat_list[0] == 2:  # if the number appears 2 times
                 current_hand_rank = repeat_list[0], value_list[0], repeat_list[1], value_list[1], self.ranks.index('one pair'), high_card
                 if current_hand_rank[4] > best_hand_rank[4]:
                     best_hand_rank = current_hand_rank
             if repeat_list[0] == 3:  # if the numbers appear 3 times
-                current_hand_rank = repeat_list[0], value_list[0], repeat_list[1], value_list[1], self.ranks.index('3 of a kind'), high_card
+                current_hand_rank = repeat_list[0], value_list[0], repeat_list[1], value_list[1], self.ranks.index('three of a kind'), high_card
                 if current_hand_rank[4] > best_hand_rank[4]:
                     best_hand_rank = current_hand_rank
             if repeat_list[0] == 4:  # if the number appear 4 times
-                best_hand_rank = repeat_list[0], value_list[0], repeat_list[1], value_list[1], self.ranks.index('4 of a kind'), high_card
+                best_hand_rank = repeat_list[0], value_list[0], repeat_list[1], value_list[1], self.ranks.index('four of a kind'), high_card
                 if current_hand_rank[4] > best_hand_rank[4]:
                     best_hand_rank = current_hand_rank
         # if there are 2 different numbers that appears in a hand more then one time
@@ -72,7 +74,7 @@ class RankDetector(object):
                 if value_list[1] > value_list[0]:  # place the number with the higher value in the hand first
                     repeat_list[1], repeat_list[0] = repeat_list[0], repeat_list[1]
                     value_list[1], value_list[0] = value_list[0], value_list[1]
-                current_hand_rank = repeat_list[0], value_list[0], repeat_list[1], value_list[1], self.ranks.index('two pair'), high_card
+                current_hand_rank = repeat_list[0], value_list[0], repeat_list[1], value_list[1], self.ranks.index('two pairs'), high_card
                 if current_hand_rank[4] > best_hand_rank[4]:
                     best_hand_rank = current_hand_rank
             if repeat_list[0] == 3:
@@ -80,6 +82,3 @@ class RankDetector(object):
                 if current_hand_rank[4] > best_hand_rank[4]:
                     best_hand_rank = current_hand_rank
         return best_hand_rank
-
-
-
