69afad4d27c59dba3e8d375d6eae10175578c1fd
OrKeren8 <orkeren8@gmail.com>
2018-02-08 13:07:26 +0200

    update euler project task 54

--- euler_projects/euler_project_task_54/euler_project_task_54.py
+++ euler_projects/euler_project_task_54/euler_project_task_54.py
@@ -21,14 +21,11 @@ def main():
             # call to the function that check the highest rank of the hand:
             left_hand_rank = check_rank(left_hand_nums, left_hand_suits)
             right_hand_rank = check_rank(right_hand_nums, right_hand_suits)
-            winner = compere_func(4, left_hand_rank, right_hand_rank)  # get the best hand
-            for i in range(5):
-                if not winner:
-                    winner = compere_func(i, left_hand_rank, right_hand_rank)
-            if winner == 'left':
-                left += 1
-            elif winner == 'right':
+            winner = compere_func(left_hand_rank, right_hand_rank)  # get the best hand
+            if winner == 'right':
                 right += 1
+            elif winner == 'left':
+                left += 1
             # clear hands for next round
             right_hand_nums.clear()
             right_hand_suits.clear()
@@ -56,24 +53,24 @@ def convert_T_J_Q_K_A_to_nums(line):
 def check_rank(hand_nums, hand_suits):  # call to the functions that gives the rank of the hand a important values
     best_rank = []  # the best case of the hand and important values on that case
     rank_class = RankDetector(hand_nums, hand_suits)
-    first_function_rank = list(rank_class.flush_straight_comb())
-    second_function_rank = list(rank_class.x_of_a_kind())
-    if first_function_rank[4] > second_function_rank[4]:  # check which function found the best rank case in a hand
+    first_function_rank = rank_class.flush_straight_comb()
+    second_function_rank = rank_class.x_of_a_kind()
+    if first_function_rank['1.rank'] > second_function_rank['1.rank']:  # check which function found the best rank case in a hand
         best_rank = first_function_rank
     else:
         best_rank = second_function_rank
     return best_rank
 
 
-def compere_func(i, left_hand_rank, right_hand_rank):
-    left_hand_rank = list(left_hand_rank)
-    right_hand_rank = list(right_hand_rank)
-    if left_hand_rank[i] > right_hand_rank[i]:
-        return 'left'
-    elif left_hand_rank[i] < right_hand_rank[i]:
-        return 'right'
-    else:
-        return 0
+def compere_func(left_hand_rank, right_hand_rank):
+    # sort the dictionary by the numbers of the keys
+    left_sorted_by_key = sorted(left_hand_rank.items())
+    right_sorted_by_key = sorted(right_hand_rank.items())
+    for i in range(6):
+        if left_sorted_by_key[i][1] > right_sorted_by_key[i][1]:
+            return 'left'
+        elif left_sorted_by_key[i][1] < right_sorted_by_key[i][1]:
+            return 'right'
 
 
 if __name__ == "__main__":
--- euler_projects/euler_project_task_54/package/detecting.py
+++ euler_projects/euler_project_task_54/package/detecting.py
@@ -4,10 +4,10 @@ class RankDetector(object):
         self.hand_nums = list(hand_nums)  # list of the numbers in the current hand
         self.hand_suits = list(hand_suits)  # list of the suits in the current hand
         self.ranks = {'high_card': 1, 'one_pair': 2, 'two_pairs': 3, 'three_of_a_kind': 4, 'straight': 5, 'flush': 6, 'full_house': 7, 'four_of_a_kind': 8, 'Straight_Flush': 9, 'royal_flush': 10}
-        self.stat = {'first_duplicate': 0, 'value1': 0, 'second_duplicate': 0, 'value2': 0, 'rank': 0, 'highCard': 0}
+        self.stat = {'2.first_duplicate': 0, '3.value1': 0, '4.second_duplicate': 0, '5.value2': 0, '1.rank': 0, '6.highCard': 0}
     # a function that takes a hand of cards and checks if there is
     # straight/flush/straight flush/ straight royal.
-    # the function returns the best case it found as it name rank
+    # the function returns the best case it found as it name 1.rank
     # and the high card of the hand
 
     def flush_straight_comb(self):
@@ -22,22 +22,22 @@ class RankDetector(object):
             if self.hand_suits[i] == self.hand_suits[i+1]:  # check if the suits in the same color
                 flush_check += 1
         if straight_check == 4 and flush_check == 4:  # of the hand has straightFlush
-            self.stat['rank'], self.stat['highCard'] = self.ranks['Straight_Flush'], high_card
+            self.stat['1.rank'], self.stat['6.highCard'] = self.ranks['Straight_Flush'], high_card
         elif (straight_check == 3) and (self.hand_nums[3] == 5) and (self.hand_nums[4] == 14) and flush_check == 4: # in case of 1,2,3,4,5 and flush
-            self.stat['rank'], self.stat['highCard'] = self.ranks['Straight_Flush'], 5
+            self.stat['1.rank'], self.stat['6.highCard'] = self.ranks['Straight_Flush'], 5
         elif flush_check == 4:  # if only flush
-            self.stat['rank'], self.stat['highCard'] = self.ranks['flush'], high_card
+            self.stat['1.rank'], self.stat['6.highCard'] = self.ranks['flush'], high_card
         elif straight_check == 4:  # if only straight
-            self.stat['rank'], self.stat['highCard'] = self.ranks['straight'], high_card
+            self.stat['1.rank'], self.stat['6.highCard'] = self.ranks['straight'], high_card
         elif (straight_check == 3) and (self.hand_nums[3] == 5) and (self.hand_nums[4] == 14):  # in a case of 1,2,3,4,5
-            self.stat['rank'], self.stat['highCard'] = self.ranks['straight'], 5
-        return self.stat.values()
+            self.stat['1.rank'], self.stat['6.highCard'] = self.ranks['straight'], 5
+        return dict(self.stat)
     
     # a function that gets a hand of cards and checks if there is
     # pair/ two pairs/ 3 of a kind/ full house/ 4 of a kind
     # the function returns the best case it found
     def x_of_a_kind(self):
-        self.stat['rank'], self.stat['highCard'] = 0, 0  # reset the stat of the hand
+        self.stat['1.rank'], self.stat['6.highCard'] = 0, 0  # reset the stat of the hand
         high_card = self.hand_nums[4]  # the last card in the sorted hand has the highest value
         repeat_list = []  # a list that gets the times a number shows in a hand
         value_list = []  # a list that get the numbers that in a hand two times or more
@@ -47,41 +47,33 @@ class RankDetector(object):
                 repeat_list.append(x)
                 value_list.append(i)
         if len(repeat_list) < 1:  # if there is no duplicate numbers
-            self.stat['highCard'] = high_card
+            self.stat['6.highCard'] = high_card
         elif len(repeat_list) < 2:  # if there is only one number that appears more then one time
             if repeat_list[0] == 2:  # if the number appears 2 times
-                self.stat['first_duplicate'], self.stat['value1'], self.stat['rank'] = repeat_list[0], value_list[0], self.ranks['one_pair']
+                self.stat['2.first_duplicate'], self.stat['3.value1'], self.stat['1.rank'] = repeat_list[0], value_list[0], self.ranks['one_pair']
             if repeat_list[0] == 3:  # if the numbers appear 3 times
-                self.stat['first_duplicate'], self.stat['value1'], self.stat['rank'] = repeat_list[0], value_list[0], self.ranks['three_of_a_kind']
+                self.stat['2.first_duplicate'], self.stat['3.value1'], self.stat['1.rank'] = repeat_list[0], value_list[0], self.ranks['three_of_a_kind']
             if repeat_list[0] == 4:  # if the number appear 4 times
-                self.stat['first_duplicate'], self.stat['value1'], self.stat['rank'] = repeat_list[0], value_list[0], self.ranks['four_of_a_kind']
+                self.stat['2.first_duplicate'], self.stat['3.value1'], self.stat['1.rank'] = repeat_list[0], value_list[0], self.ranks['four_of_a_kind']
         else:  # if there are more than two numbers that appears more than one time in a hand
             if repeat_list[1] > repeat_list[0]:  # place the number with more appearance in the hand first
                 repeat_list[1], repeat_list[0] = repeat_list[0], repeat_list[1]
                 value_list[1], value_list[0] = value_list[0], value_list[1]
             if repeat_list[0] == 3:
-                self.stat['first_duplicate'], = repeat_list[0]
-                self.stat['value1'] = value_list[0]
-                self.stat['second_duplicate'] = repeat_list[1]
-                self.stat['value2'] = value_list[1]
-                self.stat['rank'] = self.ranks['full_house']
-                self.stat['highCard'] = high_card
+                self.stat['2.first_duplicate'] = repeat_list[0]
+                self.stat['3.value1'] = value_list[0]
+                self.stat['4.second_duplicate'] = repeat_list[1]
+                self.stat['5.value2'] = value_list[1]
+                self.stat['1.rank'] = self.ranks['full_house']
+                self.stat['6.highCard'] = high_card
             else:  # if there are two pairs
                 if value_list[1] > value_list[0]:  # place the number with the higher value in the hand first
                     repeat_list[1], repeat_list[0] = repeat_list[0], repeat_list[1]
                     value_list[1], value_list[0] = value_list[0], value_list[1]
-                self.stat['first_duplicate'] = repeat_list[0]
-                self.stat['value1'] = value_list[0]
-                self.stat['second_duplicate'] = repeat_list[1]
-                self.stat['value2'] = value_list[1]
-                self.stat['rank'] = self.ranks['two_pairs']
-                self.stat['highCard'] = high_card
-        print(list(self.stat.values()))
-        return self.stat.values()
-
-
-rank_class = RankDetector([5,5,9,14,14], ['C', 'D', 'D', 'C', 'C'])
-first_function_rank = list(rank_class.flush_straight_comb())
-second_function_rank = list(rank_class.x_of_a_kind())
-print(first_function_rank)
-print(second_function_rank)
+                self.stat['2.first_duplicate'] = repeat_list[0]
+                self.stat['3.value1'] = value_list[0]
+                self.stat['4.second_duplicate'] = repeat_list[1]
+                self.stat['5.value2'] = value_list[1]
+                self.stat['1.rank'] = self.ranks['two_pairs']
+                self.stat['6.highCard'] = high_card
+        return dict(self.stat)
