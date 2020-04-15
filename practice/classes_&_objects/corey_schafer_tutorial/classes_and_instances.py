4ca1c1eee1f30545990bac8d88c8005ee2bac2eb
OrKeren8 <orkeren8@gmail.com>
2020-04-15 14:19:06 +0300

    new comment style

--- practice/classes_&_objects/corey_schafer_tutorial/classes_and_instances.py
+++ practice/classes_&_objects/corey_schafer_tutorial/classes_and_instances.py
@@ -1,30 +1,54 @@
-# set a class with a given name
+"""
+set a class with a given name
+"""
 class Employee:
 
-    # make an initialized function which happen right after creating new object of the class
+    """
+    class variable, every class will have the same variable and its value in it
+    """
+    raise_amount = 1.04
+
+    """
+    make an initialized function which happen right after creating new object of the class
+    """
     def __init__(self, first, last, pay):
 
-        # set some initialize variables which must be configure while creating a new object of this class
+        """
+        set some initialize variables which must be configure while creating a new object of this class
+        """
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
 
-    # make a usual function for the class
+    '''
+    make a usual function for the class            
+    :return: the full name by a string with the following format
+    '''
     def full_name(self):
-
-        # print the first and the last name by the given format
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
 
-# make an object of the class Employee with a given name,
-# in the Parenthesis the values of the instance variables are writen
+        """
+        the calculation of the payment of the employee is done with the class variable raise_amount
+        """
+        self.pay = int(self.pay * self.raise_amount)
+
+'''
+make an object of the class Employee with a given name,
+in the Parenthesis the values of the instance variables are writen
+'''
 emp_1 = Employee('corey', 'schemaher', 50000)
 emp_2 = Employee('or', 'keren', 1000000)
 
-# set an instance variable to the object emp_1 of Employee class
+'''
+set an instance variable to the object emp_1 of Employee class
+'''
 emp_1.some_instance_variable = 'the value of the instance variable'
 
-# print the object emp_1 of Employee class
+'''
+print the object emp_1 of Employee class
+'''
 print(emp_1)
 print(emp_2.full_name())
