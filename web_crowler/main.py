f86eb15759d3cbd6df416a786983a9c0c214b848
or keren <orkeren8@gmail.com>
2022-07-13 01:16:21 +0300

    create basic parser

--- web_crowler/main.py
+++ web_crowler/main.py
@@ -3,6 +3,16 @@ import requests
 import lxml
 
 
-source = requests.get("https://github.com/OrKeren8/practice_projects").text
+source = requests.get("https://github.com/OrKeren8/practice_projects/pulls").text
 soup = BeautifulSoup(source, 'lxml')
-print(soup.prettify())
+# print(soup.prettify())
+
+merge_requests = soup.find('div', class_="Box mt-3 Box--responsive hx_Box--firstRowRounded0")
+
+x = str(merge_requests)
+link = merge_requests.find('Issue')
+print(link)
+# for link in merge_requests.find_all('a', href=True):
+#     print(link)
+    
+# print(issue.prettify())
