fc40f3abfe14777c6158231f80422a9c45261eca
or keren <orkeren8@gmail.com>
2025-04-05 21:10:06 +0300

    finishes for the game

--- Data_science_intro/homework1/assignment/rps_game.py
+++ Data_science_intro/homework1/assignment/rps_game.py
@@ -1,16 +1,21 @@
 import json
 from typing import Dict
+import sys
 
 weights = {"rock": 2, "paper": 1, "scissors": 0}
 
 def results(results_filename):
-    with open(results_filename, 'r', encoding='utf-8') as results_file:
-        good_header = "player1 player1-choice player2 player2-choice\n"
-        if results_file.readline() != good_header:
-            raise FileExistsError("file does not have the right header")
-        for result_line in results_file:
-            result = result_line.split()
-            yield result
+    try:
+        with open(results_filename, 'r', encoding='utf-8') as results_file:
+            good_header = "player1 player1-choice player2 player2-choice\n"
+            if results_file.readline() != good_header:
+                raise FileNotFoundError("file does not have the right header")
+            for result_line in results_file:
+                result = result_line.split()
+                yield result
+    except FileNotFoundError as e:
+        print(f"file does not exist: {e}")
+        sys.exit()
 
 def game(results_filename):
     scores: Dict[str, int] = {}
@@ -22,8 +27,6 @@ def game(results_filename):
                 scores[result[2]] = scores.get(result[2], 0) + 1
             case -1 | 2:
                 scores[result[0]] = scores.get(result[0], 0) + 1
-            case 0:
-                scores["tie"] = scores.get("tie", 0) + 1
     sorted_winners = sorted(scores, key=scores.get, reverse=True)
     if scores[sorted_winners[0]] > scores[sorted_winners[1]]:
         winner = sorted_winners[0]
